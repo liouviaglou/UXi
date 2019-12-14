cfe6ee07bb91794d7fa5e07ba5987c4a64806d20
Luba Gloukhova <lubagloukhova@gmail.com>
2019-12-14 12:26:09 -0800

    finalizedish segmentation code, refined varimp output

--- scripts/user_segmentation.py
+++ scripts/user_segmentation.py
@@ -1,4 +1,8 @@
 # /usr/local/bin/python3
+
+# python3 -W ignore user_segmentation.py -nclusters 3 -a .05
+
+
 import pandas as pd
 import matplotlib
 import numpy as np
@@ -8,7 +12,7 @@ from matplotlib.backends.backend_pdf import PdfPages
 import datetime
 import os
 import itertools
-
+import argparse
 
 # Seaborn visualization library
 import seaborn as sns
@@ -32,6 +36,7 @@ import random
 random.seed( 0 )
 
 def dataprep_X (df, elbow=True, varlist=None, bystring=None):
+    
     """
     Data Prep for Model Inputs
     
@@ -62,24 +67,24 @@ def dataprep_X (df, elbow=True, varlist=None, bystring=None):
     if varlist:
         # subset dataframe
         df = df[varlist]
-        print("%d Columns loaded of %d attempted" % (df.shape[0], len(varlist)))
+#         print("%d Columns loaded of %d attempted" % (df.shape[0], len(varlist)))
 
     # coerce non-int columns to numeric
     coerce_cols = df.dtypes[df.dtypes!='int64'].index.to_list()
     df[coerce_cols] = df[coerce_cols].apply(pd.to_numeric, errors='coerce')
-    print("%d Columns coerced to numeric" % (len(coerce_cols)) )
+#     print("%d Columns coerced to numeric" % (len(coerce_cols)) )
     
     # drop columns where are values are missing 
     cols_allmissing = df.isna().sum()[df.isna().sum()==3254].index.to_list()
     df = df.drop(cols_allmissing, axis = 1) 
-    print("%d Columns dropped due to all missing values" % (len(cols_allmissing)) )
+#     print("%d Columns dropped due to all missing values" % (len(cols_allmissing)) )
     
     # Standardize the qx_list columns
     # NaNs are treated as missing values: disregarded in fit, and maintained in transform.
     scaler = StandardScaler().fit(df)
     X = df.copy()
     X[df.columns] = scaler.transform(df)
-    print("%d Columns scaled to mean=0 stdv=1" % (X.shape[0] ))
+#     print("%d Columns scaled to mean=0 stdv=1" % (X.shape[0] ))
           
     # fill NA's with 0 
     X.fillna(0, inplace=True)
@@ -102,11 +107,11 @@ def dataprep_X (df, elbow=True, varlist=None, bystring=None):
     return X
 
 # Standardization of these columns?
-def dataprep_Z (df, varlist, ohdict=None):
+def dataprep_Z (df, varmap, valmap, varlist_Z, ohdict=None):
     """
     Data Prep for attribute variables for statistical significance testing 
     
-    - 0 one hot encoding select varables
+    - 0 one hot encoding select varableZ
     (- standardization (?))
     - replaces missing values with 0
 
@@ -114,6 +119,10 @@ def dataprep_Z (df, varlist, ohdict=None):
     ----------
     df : dataframe
         data to be prepped
+    varmap : dataframe
+        varmap to be modified
+    valmap : dataframe
+        valmap to pull value Labels from
     varlist : list
         column names for Z 
     ohdict : dictionary
@@ -126,54 +135,63 @@ def dataprep_Z (df, varlist, ohdict=None):
     Returns
     -------
     Z : dataframe
+    varmap : dataframe
 
     """
     
+    # coerce non-int columns to numeric
+    coerce_cols = df.dtypes[df.dtypes!='int64'].index.to_list()
+    df[coerce_cols] = df[coerce_cols].apply(pd.to_numeric, errors='coerce')
+
+    
+    Z_rowstodrop =  []
     if ohdict:
         # one-hot encode variables
         for key, value in ohdict.items():
-            if key in varlist:
+            if key in varlist_Z:
                 if not value:
+                    varlist_Z.remove(key) 
                     for v in df[key].unique() :
                         df[key+'_'+str(v)] = np.where(df[key]==v, 1, 0)
-                        varlist.extend([key+'_'+str(v)])
-                else:
-                    print (key, value)
-                    df[key+'_X'] = ~pd.Series(df[key].isin(value))*1
-                    varlist.extend([key+'_X'])
-                    for v in value:
-                        df[key+'_'+str(v)] = np.where(df[key]==v, 1, 0)
-                        varlist.extend([key+'_'+str(v)])
-
-                varlist.remove(key)
-
-    Z = df[varlist]
+                        varlist_Z.extend([key+'_'+str(v)])
+                        
+                        try:
+                            l1 = varmap.loc[varmap.Variable==key].Label.values[0]
+                            l2 = valmap.loc[(valmap.key==key)&(valmap.id==v)].Label.values[0]
+                            Label = l1 +" "+l2
+                            keys = ['Variable', 'Label', 'Short Label', 'Type', 
+                                    'Minimum', 'Maximum', 'Description']
+                            values = [key+'_'+str(v), Label, '', '', '', '', '']
+                            varmap=varmap.append(pd.DataFrame(dict(zip(keys, values)), 
+                                                              index=[0]))
+                        except:
+                            print (key, v)
+                    
+                    
+                    
+    Z = df[varlist_Z]
     
     # fill NA's with 0 
     Z.fillna(0, inplace=True)
        
     
-    return Z
-    
+    return Z, varmap
 
-def segm_kmeans (X, n_clusters, bystring=None):
+def segm_kmeans (X, n_clusters, bystring=None, plots=False):
     if not bystring:
         bystring='{:%Y%m%d_%H%M%S}'.format(datetime.datetime.now())
     # Fitting KMeans w/ Optimal number of Clusters
     kmeans = KMeans(n_clusters=3, init='k-means++', max_iter=300, n_init=10, random_state=0)
     pred = kmeans.fit_predict(X)
     
-    with PdfPages(outroot+'/Seg1_KNN'+str(n_clusters)+'_'+bystring+'.pdf') as pdf:
-        plt.ioff()
-#         if 'pred' in X.columns:
-#             cols = X.columns.drop(['pred'])
-#         else:
-#             cols = 
-        for col in X.columns:
-            plt.figure()
-            fig=X.groupby(pred)[col].plot(kind='kde', 
-                                            title=col, legend=True)[0].get_figure()
-            pdf.savefig(fig)   
+    if plots:
+        with PdfPages(outroot+'/Seg1_KNN'+str(n_clusters)+'_'+bystring+'.pdf') as pdf:
+            plt.ioff()
+            for col in X.columns:
+                plt.figure()
+                fig=X.groupby(pred)[col].plot(kind='kde', 
+                                                title=col, legend=True)[0].get_figure()
+                pdf.savefig(fig)   
             
     return pred
     
@@ -198,14 +216,11 @@ def varimp (X, varlist, pred, c):
     
     # drop pred columns if they exist
     X = X[varlist]
-    print(X.shape)
     
     # one-hot encoding of pred == c
     ohcluster = np.where(pred==c, 1, 0)
-    print(ohcluster)
     X_vars = X.columns.values.tolist()
     y = ohcluster
-    print(y.shape)
     
     # Logistic Regression to calculate coefficients
     m = LogisticRegression()
@@ -240,29 +255,31 @@ def varimp_moreinfo (vi, df_varmap):
 
     return varimp
 
-def Zclust_stats (X, pred, Z, alpha, bystring=None):
+def Zclust_stats (X, pred, Z, alpha, bystring=None, plots=False):
     # # Examine statistical significant differences in demographics amongst segments. 
     
     clusters = np.unique(pred)
+    
     if not bystring:
         bystring='{:%Y%m%d_%H%M%S}'.format(datetime.datetime.now())
     
-    with PdfPages(outroot+'/Seg1_KNN'+str(len(clusters))+'_'+bystring+'.pdf') as pdf:
-        plt.ioff()
-        for col in Z.columns:
-            if pd.api.types.is_numeric_dtype(df_data[col]):
-                try:
-                    plt.figure()
-                    fig=Z.groupby(pred)[col].plot(kind='kde', title=col, legend=True)[0].get_figure()
-                    pdf.savefig(fig)
-                except:
-                    print("Error: "+col)
+    if plots:
+        with PdfPages(outroot+'/Seg1_KNN'+str(len(clusters))+'_'+bystring+'.pdf') as pdf:
+            plt.ioff()
+            for col in Z.columns:
+                if pd.api.types.is_numeric_dtype(df_data[col]):
+                    try:
+                        plt.figure()
+                        fig=Z.groupby(pred)[col].plot(kind='kde', title=col, legend=True)[0].get_figure()
+                        pdf.savefig(fig)
+                    except:
+                        print("Error: "+col)
 
     # Testing whether the populations have statistically sig diff in means (assuming N distr)
     # For two independent samples w/ potentially !=  variance, use Welch's t-test
     # scipy.stats.ttest_ind(cat1['values'], cat2['values'], equal_var=False)
 
-    keys = ['clusterA','clusterB','stat','pvalue','interp']
+    keys = ['Variable','clusterA','clusterB','stat','pvalue','interp']
     df_results = pd.DataFrame(columns=keys)
     for col in Z.columns:
         if pd.api.types.is_numeric_dtype(Z[col]):
@@ -292,10 +309,20 @@ def Zclust_stats (X, pred, Z, alpha, bystring=None):
                 else:
                     interp = ''
             
-                values = [ x[0], x[1], stat, pvalue, interp]
+                values = [ col, x[0], x[1], stat, pvalue, interp]
                 
                 
                 df_results = df_results.append(pd.DataFrame(dict(zip(keys, values)), index=[0]))
+
+
+    df_results = df_results.merge(df_varmap[['Variable', 'Label']], 
+                          on='Variable', how='left')
+    
+    df_results[df_results.Label=='']
+
+    df_results = df_results[['Variable','Label','interp',
+                             'clusterA','clusterB',
+                             'stat','pvalue']] 
 
                 
                 
@@ -305,8 +332,19 @@ def Zclust_stats (X, pred, Z, alpha, bystring=None):
 
 if __name__ == "__main__":
     
-    nclusters = 3
+    # arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--nclusters", "-nc", type=int,
+                        help="number of clusters for KNN")
+    parser.add_argument("--alpha", "-a", type=float,
+                        help="tolerance threshold for stat sig of attributes across clusters")
+    args = parser.parse_args()
+    print("set number of clusters for KNN %s" % args.nclusters)
+    print("tolerance threshold for stat sig of attributes across clusters %s" % args.alpha)
     
+    nclusters = args.nclusters
+    alpha =  args.alpha
+
     # Create Output Folder
     datetime='{:%Y%m%d_%H%M%S}'.format(datetime.datetime.now())
     outroot = r"../data/output/"+datetime
@@ -333,24 +371,23 @@ if __name__ == "__main__":
     varlist_Z  = [ 'd1',    'd3_1',    'd3_2',    'd3_3',    'd3_4',    'd4', 'd6',    'd7_1',    
                 'd7_2',    'd7_3',    'd7_4',    'd7_5',    'd7_97',    'd7_99',]
     
-    ohdict = {'d4' :  [7], 
+    ohdict = {'d4' :  None, 
               'd1' : None, 
               'hidagemodels' : None,
             'qxcurrentxmodel': None,
              'hbrand' : None,
              'hmodelquota' : None,
-             'd3_1': None,
              'hmodelquota_reordered' : None,
              'Empowered_Customer_Groups' : None}
 
-    Z = dataprep_Z (df_data, varlist=varlist_Z, ohdict=ohdict)
+    Z, df_varmap = dataprep_Z (df_data, df_varmap, df_valmap, varlist_Z=varlist_Z, ohdict=ohdict)
      
-    pred = segm_kmeans (X, nclusters, bystring='byattitudes')
+    pred = segm_kmeans (X, nclusters, bystring='byattitudes', plots=False)
   
-    vi = varimp (X, varlist, pred, 2)
-
-    vi_mi = varimp_moreinfo (vi, df_varmap)
-    vi_mi.to_csv(outroot+'/Seg1_KNN'+str(nclusters)+'_vimi.csv', index=False)
+    for viclust in range(nclusters):
+        vi = varimp (X, varlist, pred, viclust)
+        vi_mi = varimp_moreinfo (vi, df_varmap)
+        vi_mi.to_csv(outroot+'/Seg1_KNN'+str(nclusters)+'_vimi'+str(viclust)+'.csv', index=False)
     
-    Zcst = Zclust_stats(X, pred, Z, alpha=.05, bystring='byattributes')
+    Zcst = Zclust_stats(X, pred, Z, alpha=alpha, bystring='byattributes', plots=False)
     Zcst.to_csv(outroot+'/Seg1_KNN'+str(nclusters)+'_zclust'+str(alpha)+'.csv', index=False)
